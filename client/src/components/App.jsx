9e6d55f9e45b7159faceac92e140993f034e4043
rachel <jones.rachel.a464@gmail.com>
2021-08-23 23:21:15 -0700

    set up initial files and basics

---
+++ client/src/components/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+
+const App = () => (
+  <div>
+  <h2>Inside App!</h2>
+  <Button variant="outline-primary">A Cool Button</Button>
+  </div>
+);
+
+export default App;
---
+++ webpack.dev-config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: path.join(__dirname, '/client/src/index.jsx'),
+  module: {
+    rules: [
+      {
+        test: [/\.jsx$/],
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react', '@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.css$/i,
+        exclude: /node_modules/,
+        //use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+    ],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, '/client/dist'),
+  },
+};
+
